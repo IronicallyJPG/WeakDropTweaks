a9cca40ef24e6a5e91f1c32bd6e4ee7a9d312513
Charles Dawkins <charlesgithub@gmail.com>
2017-10-09 17:54:11 -0400

    Update WeakDropTweaks.java
    1.3 Update

--- Main/WeakDropTweaks.java
+++ Main/WeakDropTweaks.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class WeakDropTweaks
 {
     public static final String MODID = "ironic_wdt";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
